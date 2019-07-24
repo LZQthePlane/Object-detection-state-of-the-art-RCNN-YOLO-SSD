a725abb7d66208c90cfc34ca0a15103141b153b6
Zhiqiang Lee <39208670+LZQthePlane@users.noreply.github.com>
2019-07-24 15:52:11 +0800

    delete temp.py

--- YOLOv3-parse/temp.py
+++
@@ -1,9 +0,0 @@
-import keras.backend as K
-
-
-anchors = [(1, 1), (2, 2), (3, 3)]
-a = [[1, 2, 3], [4, 5, 6]]
-b = K.get_session().run(K.reshape(a, [2, 3]))
-print(b)
-anchors_tensor = K.reshape(K.constant(anchors), [1, 1, 1, 3, 2])
-print(K.constant(anchors))
